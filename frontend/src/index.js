7c9ea3b813377b19ac6ca7a39b5630f5a0ebbb3d
Haikals99 <haikals181004@gmail.com>
2026-05-16 22:22:02 +0700

    fix: pindahkan logrocket ke baris paling atas sesuai dokumentasi

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
+/* eslint-disable import/first */
+import LogRocket from 'logrocket';
+LogRocket.init('ahilgj/toko-online-mern-v2');
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import LogRocket from 'logrocket';
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import App from './App';
-
-LogRocket.init('ahilgj/toko-online-mern-v2');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
